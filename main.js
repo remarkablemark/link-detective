4ea1a346d35723587cdf3911f2c9e541db035e4d
Mark <mark@remarkablemark.org>
2016-03-27 16:22:12 -0400

    Add main that creates window and handles events
    - Based off the `electron-quick-start` repository

---
+++ main.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+const electron = require('electron');
+const app = electron.app;
+const path = require('path');
+let mainWindow;
+
+/**
+ * Create the main browser window.
+ */
+let createWindow = () => {
+    mainWindow = new electron.BrowserWindow({
+        width: 800,
+        height: 600
+    });
+    mainWindow.loadURL(path.join('file://', __dirname, 'index.html'));
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+};
+
+/**
+ * App.
+ */
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
