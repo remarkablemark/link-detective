88713430d278ff30ec160d04ce1144c65cbc211f
Mark <mark@remarkablemark.org>
2016-03-27 19:44:31 -0400

    Open DevTools in development mode
    - When `NODE_ENV=development`, open the DevTools
    - Create `npm run dev` script that sets the env variable above

--- main.js
+++ main.js
@@ -16,8 +16,11 @@ let createWindow = () => {
         width: 800,
         height: 600
     });
-
     mainWindow.loadURL(path.join('file://', __dirname, 'app/index.html'));
+
+    if (process.env.NODE_ENV === 'development') {
+        mainWindow.webContents.openDevTools();
+    }
 
     mainWindow.on('closed', () => {
         mainWindow = null;
